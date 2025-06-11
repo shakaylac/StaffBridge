820366da48c401e4fa7dd311fb610b89d353bc3b
shakayla <kaycnite@gmail.com>
2025-06-10 21:41:42 -0500

    Installed Bootstrap and tested it. Up and running

---
+++ staffbridge/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <header class="badge text-bg-primary">
+            <logo>StaffBridge</logo>
+            <button id="profile"></button>
+        </header>
+    );
+}
+
+export default Header;
